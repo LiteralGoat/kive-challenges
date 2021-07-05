72fb5a94a8881ccb3d766a1f0a00d44522bf5f6d
DANIEL VON VATTU <daniel.islandman@gmail.com>
2021-07-05 21:04:13 +0200

    conversion of images finished

--- README.md
+++ README.md
@@ -1,20 +1,36 @@
 # The Frame Extraction Challenge
-## Challenge description
+## TOC
+1. [Challenge Description](#challenge-description)
+    1. [Documentation Task](#doc-tasks)
+2. [Though Process](#thought-process)
+    1. [Starting out](#starting-out)
+    2. [Finding out what ffmpeg is](#finding-out-what-ffmpeg-is)
+    3. [Getting the video stream](#getting-the-video-stream)
+
+## Challenge Description
 - [ ] Design a JavaScript function `getYoutubeFrames()` that:
   - [X] Takes a Youtube url as its input
   - [X] Fetches the video
   - [X] Extracts a frame from every scene/cut of the video (hint: use ffmpeg)
-  - [ ] Converts every frame to jpg format with maximum 800px width
+  - [X] Converts every frame to jpg format with maximum 800px width
   - [ ] **Returns** a list of temp paths or base64 buffers
-## Documentation tasks (explain / document)
+
+### Documentation Tasks (explain / document) 
 - [ ] Subtask 1
 - [ ] Subtask 2
 - [ ] Subtask 3
 - [ ] Subtask 4
-## Starting out
+
+## Thought Process
+### **Starting out**
 First I need to find out what how to fetch a video with a url from youtube and what ffmpeg is. 
+I'll initialize an npm project and an index.js file to outline the project.
 
-I'll create a couple of files to outline the project.
-## Finding out what ffmpeg is
-It seems that ffmpeg is some sort of video and audio handler. I'll need the ability to read the file, detect scene changes and then convert the frame to a screenshot / snapshot that is that is then outputted in an ./output folder.
+### **Finding out what ffmpeg is**
+It seems that [ffmpeg](http://ffmpeg.org/) is some sort of video and audio handler. I'll need the ability to read the file, detect scene changes and then convert the frame to a screenshot / snapshot that is that is then outputted in an ./output folder.
 
+### **Getting the video stream**
+The package [ytdl](https://www.npmjs.com/package/ytdl-core) is very useful for easily getting the video from youtube, as in the challenge desc. Then I have to create an async function and return the value. 
+
+### **Creating the scene function**
+The ffmpeg filter that I could find which would detect every scene was `select='gt(scene\,0.2)'`, where the prefix was `-vf` or `-filter:v` (I used -vf for its simplicity). 
--- index.js
+++ index.js
@@ -1,36 +1,45 @@
-const fs = require('fs');
+// A youtube url fetching pkg.
 const ytdl = require('ytdl-core');
 
-// Getting the node.js ffmpeg api https://www.npmjs.com/package/fluent-ffmpeg.
+// Getting the node.js ffmpeg pkg. https://www.npmjs.com/package/fluent-ffmpeg.
 const ffmpeg = require('fluent-ffmpeg');
   // Intsalling ffmpeg https://www.npmjs.com/package/@ffmpeg-installer/ffmpeg.
   const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
   // Installing ffprobe https://www.npmjs.com/package/@ffprobe-installer/ffprobe.
   const ffprobePath = require('@ffprobe-installer/ffprobe').path;
-
-// Setting paths
-ffmpeg.setFfmpegPath(ffmpegPath);
-ffmpeg.setFfprobePath(ffprobePath);
+  // Setting paths
+  ffmpeg.setFfmpegPath(ffmpegPath);
+  ffmpeg.setFfprobePath(ffprobePath);
 
 // Gets the url parameter.
 const url = process.argv.slice(2)[0];
 
 // Fetches the video
-const stream = ytdl(url);
+/* const stream = ytdl(url) */
+async function getVideo(url) {
+  try {
+    // Quality values can be found here: https://github.com/fent/node-ytdl-core/blob/HEAD/typings/index.d.ts#L15
+    return await ytdl(url, { quality: 'lowestvideo' }); // 'highestvideo' or 'lowestvideo' as examples
+  } catch (err) {
+    console.log(err)
+  }
+}
 
-function getYoutubeFrames() {
-  console.log('URL:', url)
-  ffmpeg()
-    // Fetches the video
-    .input(stream)
+async function getYoutubeFrames() {
+  ffmpeg(await getVideo(url))
+    .size('50%')
+    // Starting message
     .on('start', () => console.log('Getting the scene thumbnails...'))
     .outputOptions([
       // Filter (vf) to select the frames to pass in output.
-      "-vf select='gt(scene\,0.2)'", // Played around with the value, 0.2 works best in my examples. Needs further discussion.
+      "-vf select='gt(scene\,0.2)',scale='min(800,iw)':-1", // Played around with the value, 0.2 works best in my examples. Needs further discussion.
       // The frame is passed through with the timestamp.
-      "-vsync vfr"
+      "-vsync vfr",
+      /* "-vf scale='min(800,iw)':-1", */
     ])
-    .output('./output/%05d.png')
+    // Outputs thumbnail as a jpg in the outputs folder
+    .output('./output/%05d.jpg')
+    // Ending message
     .on('end', () => console.log('Done! d(-_^)'))
     .run();
 };
