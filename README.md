1e8c28322a724f52e5a1257f109551df05c8eb94
DANIEL VON VATTU <daniel.islandman@gmail.com>
2021-07-05 15:53:16 +0200

    url params, fetching video, outputting scenes
    Fetch params with process.argv
    
    Fetch video with [ytdl-core](https://www.npmjs.com/package/ytdl-core) package
    
    ffmpeg
      Require [fluent-ffmpeg](https://www.npmjs.com/package/fluent-ffmpeg) package, along with installation packages [ffmpeg](https://www.npmjs.com/package/@ffmpeg-installer/ffmpeg) and [ffmprobe](https://www.npmjs.com/package/@ffprobe-installer/ffprobe)
      Extract scenes with [ffmpeg](https://ffmpeg.org/) filter
      Outputting scenes in ./output folder

--- README.md
+++ README.md
@@ -1,12 +1,20 @@
 # The Frame Extraction Challenge
 ## Challenge description
 - [ ] Design a JavaScript function `getYoutubeFrames()` that:
-  - [ ] Takes a Youtube url as its input
-  - [ ] Fetches the video
-  - [ ] Extracts a frame from every scene/cut of the video (hint: use ffmpeg)
+  - [X] Takes a Youtube url as its input
+  - [X] Fetches the video
+  - [X] Extracts a frame from every scene/cut of the video (hint: use ffmpeg)
   - [ ] Converts every frame to jpg format with maximum 800px width
   - [ ] **Returns** a list of temp paths or base64 buffers
+## Documentation tasks (explain / document)
+- [ ] Subtask 1
+- [ ] Subtask 2
+- [ ] Subtask 3
+- [ ] Subtask 4
 ## Starting out
 First I need to find out what how to fetch a video with a url from youtube and what ffmpeg is. 
 
 I'll create a couple of files to outline the project.
+## Finding out what ffmpeg is
+It seems that ffmpeg is some sort of video and audio handler. I'll need the ability to read the file, detect scene changes and then convert the frame to a screenshot / snapshot that is that is then outputted in an ./output folder.
+
--- index.js
+++ index.js
@@ -1,14 +1,38 @@
-const http = require('http');
+const fs = require('fs');
+const ytdl = require('ytdl-core');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+// Getting the node.js ffmpeg api https://www.npmjs.com/package/fluent-ffmpeg.
+const ffmpeg = require('fluent-ffmpeg');
+  // Intsalling ffmpeg https://www.npmjs.com/package/@ffmpeg-installer/ffmpeg.
+  const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
+  // Installing ffprobe https://www.npmjs.com/package/@ffprobe-installer/ffprobe.
+  const ffprobePath = require('@ffprobe-installer/ffprobe').path;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('We are up and running... d(-_^)');
-});
+// Setting paths
+ffmpeg.setFfmpegPath(ffmpegPath);
+ffmpeg.setFfprobePath(ffprobePath);
 
-server.listen(port, hostname, () => {
-  console.log(`We are up and running at http://${hostname}:${port}/ d(-_^)`);
-});
+// Gets the url parameter.
+const url = process.argv.slice(2)[0];
+
+// Fetches the video
+const stream = ytdl(url);
+
+function getYoutubeFrames() {
+  console.log('URL:', url)
+  ffmpeg()
+    // Fetches the video
+    .input(stream)
+    .on('start', () => console.log('Getting the scene thumbnails...'))
+    .outputOptions([
+      // Filter (vf) to select the frames to pass in output.
+      "-vf select='gt(scene\,0.2)'", // Played around with the value, 0.2 works best in my examples. Needs further discussion.
+      // The frame is passed through with the timestamp.
+      "-vsync vfr"
+    ])
+    .output('./output/%05d.png')
+    .on('end', () => console.log('Done! d(-_^)'))
+    .run();
+};
+
+getYoutubeFrames();
